bb248a760e4991abd080663b09e9fefda24351a9
funnykeke <qianhulei1@gmail.com>
2022-12-29 14:10:25 +0800

    first commit

---
+++ src/test/java/com/funnykeke/SpringBootHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funnykeke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
